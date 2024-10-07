65e76180eed7e54dd6bbb92df32231a4a061c2a8
ProGamerGo1234 <martin.takac47@gmail.com>
2024-10-07 14:30:25 +0400

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,18 @@
-
+let Hero = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . f f f f f f f f f f . . . 
+    . . f f f f f f f f f f f f . . 
+    . . f f f f f f f f f f f f . . 
+    . . f f f 4 4 4 4 4 4 f f f . . 
+    . . f 4 4 d f 4 4 f d 4 4 f . . 
+    . . f 4 4 1 f 4 4 f 1 4 4 f . . 
+    . . f 4 4 4 4 4 4 4 4 4 4 f . . 
+    . . f 4 4 4 4 4 4 f f 4 4 f . . 
+    . . . f 4 4 4 4 4 4 4 4 f . . . 
+    . . 4 4 f 8 8 8 8 8 8 f 4 4 . . 
+    . . 4 d f 8 8 8 8 8 8 f d 4 . . 
+    . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+    . . . . . f f f f f f . . . . . 
+    . . . . . f f . . f f . . . . . 
+    `, SpriteKind.Player)
